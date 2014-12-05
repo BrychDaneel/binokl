ad0caf363c9d1410f1fc62654fb374a1aad1ad5f
Daneel <BrychDaneel@mail.ru>
2014-12-05 21:02:45 +0300

    Тегер в процессе

--- tegger/main.pas
+++ tegger/main.pas
@@ -15,6 +15,7 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Button2: TButton;
+    Button3: TButton;
     CheckBox1: TCheckBox;
     CheckGroup1: TCheckGroup;
     ComboBox1: TComboBox;
@@ -35,6 +36,7 @@ type
     MenuItem5: TMenuItem;
     Panel1: TPanel;
     Panel2: TPanel;
+    Panel3: TPanel;
     ScrollBox1: TScrollBox;
     TreeView1: TTreeView;
     procedure Button1Click(Sender: TObject);
@@ -56,12 +58,35 @@ implementation
 { TForm1 }
 
 
+type
+  par=record
+  typ:longint;
+  comand:TStringList;
+  end;
+
+  Tteg=record
+  name:string;
+  open,close: boolean;
+  space,postspace: longint;
+  pre,post:TStringList;
+  opentagallow:TStringList;
+  npar:longint;
+  pars:array [1..100] of par;
+end;
+
+var tegs:array [1..100] of Tteg;
+  nteg:longint;
+  sot:array [1..100] of TTreeNode;
 procedure TForm1.Button1Click(Sender: TObject);
+   var it:TTreeNode;
 begin
-TreeView1.Items.AddChild(TreeView1.Selected,'<neteg>');
+it:=TreeView1.Items.AddChild(TreeView1.Selected,'<neteg>');
+inc(nteg);
+ShowMessage(IntToStr(it.AbsoluteIndex));
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
+//var index:
 begin
  ComboBox1.Items.Add(ComboBox1.Text);
 end;
